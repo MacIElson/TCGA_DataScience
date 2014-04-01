d176f393267e333024d667eed0abda84041007b8
macicrowell <macicrowell@gmail.com>
2014-03-31 23:06:22 -0400

    add the files we have so far

---
+++ DataUtilities.py
@@ -0,0 +1,170 @@
+import glob
+import numpy
+import os
+import csv
+import xml.etree.ElementTree as ET
+
+class Patient:
+    """A class for a Single Patient"""
+    mutations = []
+
+    def __init__(self, patientRootElement):
+        self.patientRootElement = patientRootElement
+
+    def addBioSpecimenRootElement(self, biospecimenRootElement):
+    	self.biospecimenRootElement = biospecimenRootElement
+
+    def getbcr_patient_barcode(self):
+    	return self.patientRootElement.find('luad:patient/shared:bcr_patient_barcode' , namespaces=getPatientXMLNameSpaces()).text
+
+    def addMutation(self, mutation):
+    	self.mutations.append(mutation)
+
+    def getAllMutations(self):
+    	return mutations
+
+def findPatientFiles(location = os.path.abspath("Data/PatientXML")):
+	"takes the location of the clinical xml files"
+	"returns a list of the file paths of all the patients"
+	print location
+	files = glob.glob(location + "/*.xml")
+	matching = [s for s in files if "_clinical" in s]
+	return matching
+
+def getPatientXMLNameSpaces():
+	namespaces = {}
+	namespaces["luad"] = "http://tcga.nci/bcr/xml/clinical/luad/2.6"
+	namespaces["rx"] = "http://tcga.nci/bcr/xml/clinical/pharmaceutical/2.6"
+	namespaces["xsi"] = "http://www.w3.org/2001/XMLSchema-instance"
+	namespaces["admin"] = "http://tcga.nci/bcr/xml/administration/2.6"
+	namespaces["shared"] = "http://tcga.nci/bcr/xml/clinical/shared/2.6"
+	namespaces["shared_stage"] = "http://tcga.nci/bcr/xml/clinical/shared/stage/2.6"
+	namespaces["lusc_luad_shared"] = "http://tcga.nci/bcr/xml/clinical/shared/lusc_luad/2.6"
+	namespaces["luad_nte"] = "http://tcga.nci/bcr/xml/clinical/luad/shared/new_tumor_event/2.6/1.0"
+	namespaces["nte"] = "http://tcga.nci/bcr/xml/clinical/shared/new_tumor_event/2.6"
+	namespaces["rad"] = "http://tcga.nci/bcr/xml/clinical/radiation/2.6"
+	namespaces["cqcf"] = "http://tcga.nci/bcr/xml/clinical/cqcf/2.6"
+	namespaces["follow_up_v1.0"] = "http://tcga.nci/bcr/xml/clinical/luad/followup/2.6/1.0"
+	return namespaces
+
+def getBiospecimenXMLNameSpaces():
+	namespaces = {}
+	namespaces["bio"] = "http://tcga.nci/bcr/xml/biospecimen/2.6"
+	namespaces["admin"] = "http://tcga.nci/bcr/xml/administration/2.6"
+	namespaces["bio_shared"] = "http://tcga.nci/bcr/xml/biospecimen/shared/2.6"
+	namespaces["cqcf"] = "http://tcga.nci/bcr/xml/clinical/cqcf/2.6"
+	namespaces["shared"] = "http://tcga.nci/bcr/xml/clinical/shared/2.6"
+	namespaces["xsi"] = "http://www.w3.org/2001/XMLSchema-instance"
+
+	return namespaces
+
+def getPatientsWithDrugsLocation(location = os.path.abspath("Data/PatientXML")):
+	patientsWithDrugsList = []
+	for patient in findPatientFiles(location):
+		tree = ET.parse(patient)
+		rootElement = tree.getroot()
+
+		for drugs in rootElement.findall("luad:patient/rx:drugs", namespaces=getPatientXMLNameSpaces()):
+			if len(drugs) != 0:
+				patientsWithDrugsList.append(patient)
+	return patientsWithDrugsList
+
+def getPatientWithDrugBarcodes():
+	barcodeList = []
+	for patient in getPatientsWithDrugsXMLTrees():
+		barcodeList.append(patient.find('luad:patient/shared:bcr_patient_barcode' , namespaces=getPatientXMLNameSpaces()).text)
+	return barcodeList
+
+def getPatientsWithDrugsXMLTrees(location = os.path.abspath("Data/PatientXML")):
+	patientsWithDrugsList = []
+	for patient in findPatientFiles(location):
+		tree = ET.parse(patient)
+		rootElement = tree.getroot()
+
+		for drugs in rootElement.findall("luad:patient/rx:drugs", namespaces=getPatientXMLNameSpaces()):
+			if len(drugs) != 0:
+				patientsWithDrugsList.append(rootElement)
+	return patientsWithDrugsList
+
+def getTrueDrugName(drugName):
+	if drugName == 'cisplatinum' or drugName == 'cisplastin':
+		drugName = 'cisplatin'
+	elif drugName == 'carboplatinum':
+		drugName = 'carboplatin'
+	elif drugName == 'altima' or drugName == 'pemetrexed disodium' or drugName == 'pemethexed' or drugName == 'alimta' or drugName == 'premetrexed':
+		drugname = 'pemetrexed'
+	elif drugName == 'gemzar':
+		drugName = 'gemcitabine'
+	elif drugName == 'tarceva (erlotinib)' or drugName == 'tarceva':
+		drugName = 'erlotonib'
+	elif drugName == 'vinorelbine tartrate' or drugName == 'vinorelbin' or drugName == 'navalbine':
+		drugName = 'vinorelbine'
+	elif drugName == 'vepesid':
+		drugName = 'etoposide'
+	elif drugName == 'abraxane' or drugName == 'taxol':
+		drugName = 'paclitaxel'
+	elif drugName == 'docetoxel/taxotere' or drugName == 'taxotere':
+		drugName = 'docetaxel'
+	elif drugName == 'chemo, multi-agent, nos':
+		drugName = 'chemo, nos'
+	elif drugName == 'rec mage 3-as + as15 acs1 / placebo vaccine' or drugName == 'recprame+as15 asci':
+		drugName = 'placebo'
+	elif drugName == 'avastin':
+		drugName = 'bevacizumab'
+	return drugName
+
+def getBiospecimenRootElement(patient_barcode):
+	location = os.path.abspath("Data/BiospecimenXML")
+	bioFiles = glob.glob(location + "/*.xml")
+	matching = [s for s in bioFiles if patient_barcode in s]
+	tree = ET.parse(matching[0])
+	rootElement = tree.getroot()
+	return rootElement
+
+def getListOfPatientObjects():
+	PatientList = []
+	for PatientXML in getPatientsWithDrugsXMLTrees():
+		patient = Patient(PatientXML)
+		patient.addBioSpecimenRootElement(getBiospecimenRootElement(patient.getbcr_patient_barcode()))
+		PatientList.append(patient)
+	return PatientList
+
+def getDictionaryOfPatients():
+	"""get a dictionary of Patients with their barcodes as keys"""
+	patientDict = {}
+	patientList = getListOfPatientObjects()
+	for patient in patientList:
+		patientDict[patient.getbcr_patient_barcode()] = patient
+	return patientDict
+
+class Mutation:
+    """A class for a Single Mutation"""
+    def __init__(self, mutationRow):
+        self.mutationRow = mutationRow
+
+def getListofMutations():
+	"""get a List of Mutations for patients that have drug data"""
+	ifile  = open(os.path.abspath("Data/broad.mit.edu__Illumina_Genome_Analyzer_DNA_Sequencing_level2.csv"), "rb")
+	reader = csv.reader(ifile, delimiter='	')
+	patientBarcodeList = getPatientWithDrugBarcodes()
+	mutationList = []
+	for row in reader:
+	    if row[15][0:12] in patientBarcodeList:
+	    	mutation = Mutation(row)
+	    	mutationList.append(mutation)
+
+def getListofPatients():
+	"""returns a list of Patients that have drug information, their patient xml tree, biospecimen xml tree and a list of mutations"""
+	ifile  = open(os.path.abspath("Data/broad.mit.edu__Illumina_Genome_Analyzer_DNA_Sequencing_level2.csv"), "rb")
+	reader = csv.reader(ifile, delimiter='	')
+	patientBarcodeList = getPatientWithDrugBarcodes()
+	patientDict = getDictionaryOfPatients()
+	print patientDict
+	mutationList = []
+	for row in reader:
+	    if row[15][0:12] in patientBarcodeList:
+	    	mutation = Mutation(row)
+	    	patientDict[row[15][0:12]].addMutation(mutation)
+	return patientDict.items()
+
+getListofPatients()
---
+++ playingWithData.py
@@ -0,0 +1,103 @@
+import glob
+import xml.etree.ElementTree as ET
+import thinkplot
+import DataUtilities
+import numpy
+
+matching = DataUtilities.findPatientFiles()
+namespaces = DataUtilities.getPatientXMLNameSpaces()
+
+drugList = {}
+patientsWithDrugs = 0
+drugsPerPatient = []
+
+patientWithPlat = 0
+
+"getDataOverview"
+for patient in matching:
+	tree = ET.parse(patient)
+	rootElement = tree.getroot()
+	plat = False
+	for drugs in rootElement.findall("luad:patient/rx:drugs", namespaces=namespaces):
+		if len(drugs) != 0:
+			drugsPerPatient.append(len(drugs))
+			patientsWithDrugs += 1
+			for drug in drugs:
+				drugName = drug.find('rx:drug_name' , namespaces=namespaces).text
+				if drugName:
+					drugName = DataUtilities.getTrueDrugName(drugName.lower())
+					# I think this is where FixingErrors.py should go.
+					if drugName.lower() == "cisplatin" or drugName.lower() == "carboplatin":
+						plat = True
+					print (drugName)
+					if drugName.lower() in  drugList:
+						drugList[drugName.lower()] += 1
+					else:
+						drugList[drugName.lower()] = 1
+			#print (drug.tag, drug.attrib)
+	if plat:
+		patientWithPlat += 1
+print drugList
+print patientWithPlat
+
+# print patientsWithDrugs
+# print sum(drugList.itervalues())
+# drugsPerPatientHist = thinkstats2.MakeHistFromList(drugsPerPatient)
+# thinkplot.Hist(drugsPerPatientHist)
+# thinkplot.Save(root='Distribution_of_Drugs_per_Patient',
+# 			title='Distribution of Drugs per Patient',
+#           xlabel='Number of Drugs taken',
+#            ylabel='NUmber of Patients')
+# thinkplot.Show(title='Distribution of Drugs per Patient',
+#         	xlabel='Number of Drugs taken', 
+#         	ylabel='NUmber of Patients')
+
+patientsWithDrugs = DataUtilities.getPatientsWithDrugsLocation(location = "/home/apatterson1/Desktop/XML")
+
+# "Death Distribution"
+# c=0
+# noFollowUps = 0
+# daysToDeathList = []
+# "days to death"
+# for patient in patientsWithDrugs:
+# 	tree = ET.parse(patient)
+# 	rootElement = tree.getroot()
+
+# 	patientDaysToDeathListStr = []
+# 	for follow_ups in rootElement.findall("luad:patient/luad:follow_ups", namespaces=namespaces):
+# 		if len(follow_ups) == 0:
+# 			noFollowUps += 1
+# 		for follow_up in follow_ups:
+# 			daysToDeath = follow_up.find("shared:days_to_death", namespaces=namespaces).text
+# 			if daysToDeath is not None:
+# 				c += 1
+# 				patientDaysToDeathListStr.append(daysToDeath)
+# 				break
+# 	print(patientDaysToDeathListStr)
+# 	if len(patientDaysToDeathListStr) != 0:
+# 		averageDaysToDeath = numpy.mean(map(int, patientDaysToDeathListStr))
+# 		daysToDeathList.append(averageDaysToDeath)
+# print "noFollowUps", noFollowUps
+# print "DaysToDeath", len(daysToDeathList)
+# Binned_Data = DataUtilities.roundToYear(daysToDeathList)
+# DaysToDeathHist = thinkstats2.MakeHistFromList(Binned_Data)
+# thinkplot.Hist(DaysToDeathHist)
+# thinkplot.Save(root='Distribution_of_Days_to_Death',
+# 			title='Distribution of Days to Death',
+#         	xlabel='Years to Death',
+#         	ylabel='Patient Count')
+# thinkplot.Show(title='Distribution of Days to Death',
+#         	xlabel='Years to Death',
+#         	ylabel='Patient Count')
+# VitalStatusList = {}
+# for patient in patientsWithDrugs:
+# 	tree = ET.parse(patient)
+# 	rootElement = tree.getroot()
+# 	vitalStatus = rootElement.find("luad:patient/shared:vital_status",namespaces=namespaces).text
+# 	if vitalStatus:
+# 		if vitalStatus.lower() in  VitalStatusList:
+# 			VitalStatusList[vitalStatus.lower()] += 1
+# 		else:
+# 			VitalStatusList[vitalStatus.lower()] = 1
+# print VitalStatusList
+# thinkplot.Hist(VitalStatusHist)
