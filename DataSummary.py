abaed7ca2e652da0d30af5d599692f9a208fcf8d
macicrowell <macicrowell@gmail.com>
2014-04-01 01:42:55 -0400

    added data Summaries for some fields

--- DataSummary.py
+++ DataSummary.py
@@ -6,6 +6,8 @@ def getSummaryGender(patientList=None):
 	dataDict = {}
 	for patient in patientList:
 		data = patient. getGender()
+		if data == None:
+			data = "Unknown"
 		if data in dataDict.keys():
 			dataDict[data] += 1
 		else:
@@ -14,5 +16,88 @@ def getSummaryGender(patientList=None):
 	for key, value in dataDict.iteritems():
 		print key + ": " + str(value)
 	print ""
+	return dataDict
 
-getSummaryGender()
+def getSummaryPrior_dx(patientList=None):
+	if patientList is None:
+		patientList = DataUtilities.getListofPatients()
+	dataDict = {}
+	for patient in patientList:
+		data = patient.getPrior_dx()
+		if data == None:
+			data = "Unknown"
+		if data in dataDict.keys():
+			dataDict[data] += 1
+		else:
+			dataDict[data] = 1
+	print "Prior_dx Breakdown"
+	for key, value in dataDict.iteritems():
+		print key + ": " + str(value)
+	print ""
+	return dataDict
+
+def getSummaryRace(patientList=None):
+	if patientList is None:
+		patientList = DataUtilities.getListofPatients()
+	dataDict = {}
+	for patient in patientList:
+		data = patient.getRace()
+		if data == None:
+			data = "Unknown"
+		if data in dataDict.keys():
+			dataDict[data] += 1
+		else:
+			dataDict[data] = 1
+	print "Race Breakdown"
+	for key, value in dataDict.iteritems():
+		print key + ": " + str(value)
+	print ""
+	return dataDict
+
+def getSummaryEthnicity(patientList=None):
+	if patientList is None:
+		patientList = DataUtilities.getListofPatients()
+	dataDict = {}
+	for patient in patientList:
+		data = patient.getEthnicity()
+		if data == None:
+			data = "Unknown"
+		if data in dataDict.keys():
+			dataDict[data] += 1
+		else:
+			dataDict[data] = 1
+	print "Ethnicity Breakdown"
+	for key, value in dataDict.iteritems():
+		print key + ": " + str(value)
+	print ""
+	return dataDict
+
+def getSummaryPathologic_stage(patientList=None):
+	if patientList is None:
+		patientList = DataUtilities.getListofPatients()
+	dataDict = {}
+	for patient in patientList:
+		data = patient.getPathologic_stage()
+		if data == None:
+			data = "Unknown"
+		if data in dataDict.keys():
+			dataDict[data] += 1
+		else:
+			dataDict[data] = 1
+	print "Pathologic_stage Breakdown"
+	for key, value in dataDict.iteritems():
+		print key + ": " + str(value)
+	print ""
+	return dataDict
+
+
+def getAllSummary():
+	patientList = DataUtilities.getListofPatients()
+	getSummaryGender(patientList)
+	getSummaryPrior_dx(patientList)
+	getSummaryRace(patientList)
+	getSummaryEthnicity(patientList)
+	getSummaryPathologic_stage(patientList)
+	getSummaryClinical_stage(patientList)
+
+getAllSummary()
